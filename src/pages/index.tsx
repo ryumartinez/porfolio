2dce114a39993287cc30cb9607cabab32e0521a0
ryumartinez <ryueduardomartinez@outlook.es>
2023-01-27 20:10:54 -0300

    cambiando imagenes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,7 @@ import deved from "../../public/dev-ed-wave.png"
 import design from "../../public/design.png"
 import code from "../../public/code.png"
 import consulting from "../../public/consulting.png"
-import web1 from "../../public/web1.png"
-import web2 from "../../public/web2.png"
-import web3 from "../../public/web3.png"
-import web4 from "../../public/web4.png"
-import web5 from "../../public/web5.png"
-import web6 from "../../public/web6.png"
-import { useState } from "react";
+import houseofdev from "../../public/houseofdev.png"
 
 const Home: NextPage = () => {
   return (
@@ -89,24 +83,13 @@ aprendí.</p>
         <h3 className="text-3xl py-1">Portfolio</h3>
           
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image src={web1} alt="web1"/>
-            </div>
+           
              <div className="basis-1/3 flex-1 ">
-              <Image src={web2} alt="web1" className="rounded-lg object-cover w-full h-full"/>
+              <a href="https://houseofdev.netlify.app/#/home">
+              <Image src={houseofdev} alt="web1" className="rounded-lg object-cover w-full h-full"/>
+              </a>
             </div>
-             <div className="basis-1/3 flex-1 ">
-              <Image src={web3} alt="web1" className="rounded-lg object-cover w-full h-full"/>
-            </div>
-             <div className="basis-1/3 flex-1 ">
-              <Image src={web4} alt="web1" className="rounded-lg object-cover w-full h-full"/>
-            </div>
-             <div className="basis-1/3 flex-1 ">
-              <Image src={web5} alt="web1" className="rounded-lg object-cover w-full h-full"/>
-            </div>
-             <div className="basis-1/3 flex-1 ">
-              <Image src={web6} alt="web1" className="rounded-lg object-cover w-full h-full"/>
-            </div>
+            
 
           </div>
       </section>
